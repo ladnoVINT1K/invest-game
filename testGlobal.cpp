aeeafe4981e73c7198e3080faf2ca0634d13305e
ladnoVINT1K <poiiir985@gmail.com>
2025-10-20 21:14:34 +0300

    Cmake

--- testGlobal.cpp
+++ testGlobal.cpp
@@ -3,17 +3,13 @@
 
 signed main(void) {
     GameAPI api;
-    api.buy("GOLD", 10000);
-    api.buy("SILVER", 1000);
-    auto Predict = api.predictPrice("GOLD", 3);
-    api.nextStep();
-    api.nextStep();
-    api.nextStep();
-    
+    api.buy("BOND", 1000);
+    auto Predict = api.predictPrice("BOND", 3);
+    for (int i = 0; i < 12; ++i) api.nextStep();
+    api.sell("BOND", 20000);
     std::cout << "Symb:" << Predict.symbol << " | " 
               << Predict.min << " | " 
               << Predict.max << " | " 
               << Predict.expected << std::endl;
-    std::cout << api.getCurrentPrice("GOLD"); 
-    
+    std::cout << api.getCurrentPrice("BOND"); 
 }
